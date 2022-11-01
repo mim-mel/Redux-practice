6d224408466dbb21beca859f674d162c70a23e9c
mim-mel <mfblade2014@gmail.com>
2022-11-01 17:51:39 +0900

    feat: todos 프로젝트 js파일에 reducer와 store추가

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+import { createStore } from 'redux';
+
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
