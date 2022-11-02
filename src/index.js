4e8ad5ac84fcd888a22710f18868d7b9bd38fbee
mim-mel <mfblade2014@gmail.com>
2022-11-02 13:44:00 +0900

    feat: todo 프로젝트에 vanilla js와 redux를 활용하여 todo 추가 및 삭제 기능 생성

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import { createStore } from 'redux';
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
 
 const ADD_TODO = 'ADD_TODO';
 const DELETE_TODO = 'DELETE_TODO';
 
+const addTodo = text => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+
+const deleteTodo = id => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
-      return [];
+      return state.filter(toDo => toDo.id !== action.id);
     default:
       return state;
   }
@@ -16,21 +33,37 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-const ul = document.querySelector('ul');
+const dispatchAddTodo = text => {
+  store.dispatch(addTodo(text));
+};
 
-const createTodo = toDo => {
-  const li = document.createElement('li');
-  li.innerText = toDo;
-  ul.appendChild(li);
+const dispatchDeleteTodo = e => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteTodo(id));
 };
 
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = '';
-  createTodo(toDo);
+  dispatchAddTodo(toDo);
 };
+
+const paintTodos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = '';
+  toDos.forEach(toDo => {
+    const li = document.createElement('li');
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    ul.appendChild(li);
+    const btn = document.createElement('button');
+    btn.innerText = 'DEL';
+    btn.addEventListener('click', dispatchDeleteTodo);
+    li.appendChild(btn);
+  });
+};
+
+store.subscribe(paintTodos);
 
 form.addEventListener('submit', onSubmit);
