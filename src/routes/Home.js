17e503063cf17ea81204975585fc79b621e8543c
mim-mel <mfblade2014@gmail.com>
2022-11-08 23:37:08 +0900

    feat: Home 컴포넌트에 투두리스트를 위한 input 작성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+
 const Home = () => {
+  const [text, setText] = useState('');
+
+  const onChange = e => {
+    setText(e.target.value);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(text);
+  };
+
   return (
     <div>
-      <h2>Home 페이지</h2>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type='text' value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
     </div>
   );
 };
