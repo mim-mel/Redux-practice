83df88f5f1b2e99941c10e93ef686d5bea257254
mim-mel <mfblade2014@gmail.com>
2022-11-14 17:37:38 +0900

    feat: (리액트 리덕스를 툴킷으로 바꾸기) reducer를 createReducer로 대체하기

--- src/store.js
+++ src/store.js
@@ -1,19 +1,29 @@
 import { createAction } from '@reduxjs/toolkit';
-import { createStore } from '@reduxjs/toolkit';
+import { createReducer } from '@reduxjs/toolkit';
+import { createStore } from 'redux';
 
 const addToDo = createAction('ADD');
 const deleteToDo = createAction('DELETE');
 
-const reducer = (state = [{ text: 'hello', id: Date.now() }], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter(toDo => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+// const reducer = (state = [{ text: 'hello', id: Date.now() }], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter(toDo => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) => {
+    state.filter(toDo => toDo.id !== action.payload);
+  },
+});
 
 const store = createStore(reducer);
 
